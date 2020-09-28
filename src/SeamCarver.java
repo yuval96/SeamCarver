6c3370beddd2d454c09ce373e84a7991909b6c05
Yuval Hayam <12692573+yuval96@users.noreply.github.com>
2020-09-28 17:19:01 +0300

    energy method done

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -29,20 +29,42 @@ public class SeamCarver {
     }
 
     // energy of pixel at column x and row y
-    public double energy(int x, int y)
+    public double energy(int x, int y) {
+        if (x == 0 || y == 0) {
+            return 1000;
+        }
+        if (x >= pic.width() || y >= pic.height()) {
+            throw new IllegalArgumentException("x or y are out of range");
+        }
 
-    // sequence of indices for horizontal seam
-    public int[] findHorizontalSeam()
+        double dX = deltaX(x,y);
+        double dY = deltaY(x, y);
 
-    // sequence of indices for vertical seam
-    public int[] findVerticalSeam()
+        return Math.sqrt(dX + dY);
+    }
 
-    // remove horizontal seam from current picture
-    public void removeHorizontalSeam(int[] seam)
+    private double deltaX(int x, int y) {
+        // get encoded rgb and decode to r, g, b separately.
+        int rgbRight = pic.getRGB(x+1, y);
+        int rgbLeft = pic.getRGB(x-1, y);
+        int r = ((rgbRight >> 16) & 0xFF) - ((rgbLeft >> 16) & 0xFF);
+        int g = ((rgbRight >>  8) & 0xFF) - - ((rgbLeft >> 8) & 0xFF);
+        int b = (rgbRight & 0xFF) - (rgbLeft & 0xFF);
 
-    // remove vertical seam from current picture
-    public void removeVerticalSeam(int[] seam)
+        // return r^2 + g^2 + b^2
+        return (Math.pow(r,2) + Math.pow(g,2) + Math.pow(b,2));
+    }
 
-    //  unit testing (optional)
-    public static void main(String[] args)
+    private double deltaY(int x, int y) {
+        // get encoded rgb and decode to r, g, b separately.
+        int rgbRight = pic.getRGB(x, y+1);
+        int rgbLeft = pic.getRGB(x, y-1);
+        int r = ((rgbRight >> 16) & 0xFF) - ((rgbLeft >> 16) & 0xFF);
+        int g = ((rgbRight >>  8) & 0xFF) - - ((rgbLeft >> 8) & 0xFF);
+        int b = (rgbRight & 0xFF) - (rgbLeft & 0xFF);
+
+        // return r^2 + g^2 + b^2
+        return (Math.pow(r,2) + Math.pow(g,2) + Math.pow(b,2));
+    }
+
 }
