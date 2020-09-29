ce47ae957d4c3b441dfd42f53250216429a3f643
Yuval Hayam <12692573+yuval96@users.noreply.github.com>
2020-09-29 16:23:25 +0300

    method findVerticalSeam done and tested

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -3,6 +3,7 @@ import edu.princeton.cs.algs4.Picture;
 public class SeamCarver {
 
     private final Picture pic;
+    static final double BORDER = 1000.0;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
@@ -31,7 +32,7 @@ public class SeamCarver {
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
         if (x == 0 || y == 0 || x == pic.width()-1 || y == pic.height()-1) {
-            return 1000;
+            return BORDER;
         }
         if (x >= pic.width() || y >= pic.height()) {
             throw new IllegalArgumentException("x or y are out of range");
@@ -41,6 +42,70 @@ public class SeamCarver {
         double dY = deltaY(x, y);
 
         return Math.sqrt(dX + dY);
+    }
+
+/*    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        // transpose and call findVerticalSeam
+
+    }*/
+
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        double[][] energyMatrix = picToMatrix();
+        int[][] edgeTo = new int[pic.height()][pic.width()];
+        double[][] distTo = new double[pic.height()][pic.width()];
+
+        int height = pic.height();
+        int width = pic.width();
+
+        // initialize matrices
+        for (int x = 0; x < width; x++) {
+            for (int y=0; y < height; y++) {
+                if (y == 0) {
+                    edgeTo[y][x] = x;
+                    distTo[y][x] = BORDER;
+                }
+                else {
+                    distTo[y][x] = Double.POSITIVE_INFINITY;
+                    edgeTo[y][x] = -1;
+                }
+            }
+        }
+
+        // topological order
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height-1; y++) {
+                for (int i = -1; i <= 1; i++) {
+                    // border, there are only two options to check
+                    if (x + i < 0 || x + i == width) {
+                        continue;
+                    }
+                    if (distTo[y][x] + energyMatrix[y+1][x + i] < distTo[y+1][x]) {
+                        distTo[y+1][x] = distTo[y][x] + energyMatrix[y+1][x+i];
+                        edgeTo[y+1][x] = x + i;
+                    }
+                }
+            }
+        }
+
+        // find shortest path
+        double min = distTo[height-1][0];
+        int minIndex = 0;
+        for (int x = 1; x < width; x++) {
+            if (distTo[height-1][x] < min) {
+                min = distTo[height-1][x];
+                minIndex = x;
+            }
+        }
+
+        // create the path[]
+        int[] path = new int[height];
+        for (int y = height-1; y >= 0; y--) {
+            path[y] = edgeTo[y][minIndex];
+        }
+
+        return path;
     }
 
     private double deltaX(int x, int y) {
@@ -65,6 +130,17 @@ public class SeamCarver {
 
         // return r^2 + g^2 + b^2
         return (Math.pow(r,2) + Math.pow(g,2) + Math.pow(b,2));
+    }
+
+    private double[][] picToMatrix() {
+        double[][] energyMatrix = new double[pic.height()][pic.width()];
+
+        for (int row = 0; row < pic.height(); row++) {
+            for (int col = 0; col < pic.width(); col++) {
+                energyMatrix[row][col] = energy(col, row);
+            }
+        }
+        return energyMatrix;
     }
 
 }
